4a9bfa0a418f697b6a8f8635367102e1ff8be7be
ruslanepi <ruslanep2@gmail.com>
2021-06-21 23:59:40 +0300

    added new files about hooks

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import TopBar from 'components/topBar';
 
 import Routes from 'Routes';
 
 const App = () => {
   return (
     <div>
-      <div>Welcome to hooks</div>
-      <Routes />
+      <Router>
+        <TopBar />
+        <Routes />
+      </Router>
     </div>
   );
 };
