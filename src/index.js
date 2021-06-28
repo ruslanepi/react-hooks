82bab9946d276ea72ace42f6c886b99d21ca9ff6
ruslanepi <ruslanep2@gmail.com>
2021-06-28 21:11:46 +0300

    added navlinks for logged & unlogged users

--- src/index.js
+++ src/index.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
+import {BrowserRouter as Router} from 'react-router-dom';
 import TopBar from 'components/topBar';
 
 import Routes from 'Routes';
-import { CurrentUserProvider } from 'contexts/currentUser';
+import {CurrentUserProvider} from 'contexts/currentUser';
+import CurrentUserChecker from 'components/CurrentUserChecker';
 
 const App = () => {
-  return (
-    <CurrentUserProvider>
-      <Router>
-        <TopBar />
-        <Routes />
-      </Router>
-    </CurrentUserProvider>
-  );
+    return (
+        <CurrentUserProvider>
+            <CurrentUserChecker>
+                <Router>
+                    <TopBar/>
+                    <Routes/>
+                </Router>
+            </CurrentUserChecker >
+        </CurrentUserProvider>
+    );
 };
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <App/>, document.getElementById('root'));
